4a9c4975b458706508000ad8f025b3ba3374a0e3
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-15 07:15:20 -0700

    workig with multipleroutes

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+#home
 @app.get("/")
 def home():
     return{"message":"Hello fastapi with vern"}
+
+#About
+@app.get("/about")
+def about():
+    return{"message":"this is about page"}
+
+#Users
+@app.get("/users")
+def users():
+    return{
+        "users":["Ahmad","Roumesa","Hamza"]
+    }
