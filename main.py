d1ffd117fb39b0652bcd6f59bc5d7222a79e25b3
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-16 03:40:36 -0700

    pydantic fast api killer feature

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name:str
+    age:int
 #Users
 @app.post("/create_users")
-def create_user(user:dict):
+def create_user(user:User):
     return{
         "message":"user created",
         "data":user
