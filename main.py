6fb628b0a8db7e004b134964ed399078d8b19c15
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-16 03:30:50 -0700

    request body post request

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 #Users
-@app.get("/users")
-def get_user(name: str=None):
-    return{"Name": name}
+@app.post("/create_users")
+def create_user(user:dict):
+    return{
+        "message":"user created",
+        "data":user
+           }
 
-#Default value
-@app.get("/products")
-def get_user(limit: int=10):
-    return{"limit": limit}
-
-#Mulitiquery parmas
-@app.get("/items")
-def get_user(name: str=None , price: int=10):
-    return{"name": name,
-          "price": price
-          }
 
