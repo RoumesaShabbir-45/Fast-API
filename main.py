cc81ed673663056c06e93fd71b902e077117614b
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-16 04:13:19 -0700

    nested model

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# class User(BaseModel):
+#     name:str
+#     age:int
+#     email:str
+# #Users
+# @app.post("/create_users")
+# def create_user(user:User):
+#     return{
+#         "message":"user created",
+#         "data":user
+#            }
+
+class Address(BaseModel):
+    city:str
+    pincode:int
+
 class User(BaseModel):
     name:str
     age:int
-    email:str
-#Users
-@app.post("/create_users")
-def create_user(user:User):
+    address:Address
+
+@app.post("/created_user")
+def created_user(user:User):
     return{
-        "message":"user created",
-        "data":user
-           }
-
-
+        user
+    }
