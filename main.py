8f95e28c7a59c48c7dc20c35a99837a1cbccfd1e
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-17 06:45:27 -0700

    dependency injection

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-from fastapi import FastAPI, status, HTTPException,Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Depends,Header, HTTPException
 
+app=FastAPI()
 
-app = FastAPI()
-class UserNotFoundException(Exception):
-  def __init__(self,name:str):
-   self.name = name
-
-@app.exception_handler(UserNotFoundException)
-def user_not_found_handler(request:Request,exc:UserNotFoundException):
-  return JSONResponse(
-    status_code=404,
-    content={
-      "status":"error",
-      "message":f"User{exc.name} not found"
-    }
+#auth to handle token
+def varify_token(token:str = Header(None)):
+ if token != "myscreatkey": 
+  raise HTTPException(
+   status_code=401,
+   detail="Unauthorized"
   )
+ return{
+  "user":"authorized user"
+ }
+@app.get("/secure_data")
+def secure_data(user=Depends(varify_token)):
+ return{
+  "message":"secure data accessed",
+  "user":user
+ }
 
-@app.get("/users/{name}")
-def get_user(name:str):
-  if name!="mohit":
-    raise UserNotFoundException(name)
-  return{
-    "name":name
-  }
+#reuseable logic
+# def get_current_user():
+#     return{
+#         "user":"mohit"
+#     }
+# @app.get("/profile")
+# def profile(user= Depends(get_current_user)):
+#     return user
 
+# @app.get("/dashboard")
+# def dashboard(user= Depends(get_current_user)):
+#     return user
+  
 
-
-
-
-# @app.get("/users/{user_id}")
-# def get_user(user_id:int):
-#   if user_id !=1:
-#     raise HTTPException(
-#       status_code= 404,
-#       detail="user not found"
-#     )
-#   return{
-#     "id":"1",
-#     "name":"mohit"
-#   }
+#Depends logic
+# def common_logic():
+#     return{
+#         "meassage":"common logic executed"
+#     }
+# @app.get("/home")
+# def home(data = Depends(common_logic)):
+#     return data
