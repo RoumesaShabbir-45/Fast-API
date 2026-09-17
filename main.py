4b0a5c20b30d8a94c51bb41e61f7f38a00e30917
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-17 07:54:47 -0700

    middleware,logging middleware

--- main.py
+++ main.py
@@ -1,43 +1,24 @@
-from fastapi import FastAPI, Depends,Header, HTTPException
-
+from fastapi import FastAPI, Request
+import time
 app=FastAPI()
 
-#auth to handle token
-def varify_token(token:str = Header(None)):
- if token != "myscreatkey": 
-  raise HTTPException(
-   status_code=401,
-   detail="Unauthorized"
-  )
- return{
-  "user":"authorized user"
- }
-@app.get("/secure_data")
-def secure_data(user=Depends(varify_token)):
- return{
-  "message":"secure data accessed",
-  "user":user
- }
+#loging middleware
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+ start_time= time.time()
+ response= await call_next(request)
+ process_time= time.time() - start_time
+ print(f"path:{request.url.path} | Time:{process_time}")
+ return response
 
-#reuseable logic
-# def get_current_user():
-#     return{
-#         "user":"mohit"
-#     }
-# @app.get("/profile")
-# def profile(user= Depends(get_current_user)):
-#     return user
 
-# @app.get("/dashboard")
-# def dashboard(user= Depends(get_current_user)):
-#     return user
-  
 
-#Depends logic
-# def common_logic():
-#     return{
-#         "meassage":"common logic executed"
-#     }
-# @app.get("/home")
-# def home(data = Depends(common_logic)):
-#     return data
+
+# @app.middleware("http")
+# async def my_middleware(request:Request,call_next):
+#     print("Request Received")
+
+#     response= await call_next(request)
+#     print("Response Sent")
+#     return response
+
