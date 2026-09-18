f628c41d94750726f6259301ff44ccdfde8c94a9
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-18 06:04:35 -0700

    database SQLAlchemy

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-from fastapi import FastAPI
-import sqlite3
+from fastapi import FastAPI,Depends
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import sessionmaker,declarative_base,Session
 
 app=FastAPI()
 
-conn= sqlite3.connect("test.db",check_same_thread= False)
-cursor= conn.cursor()
-
-cursor.execute(""" 
-CREATE TABLE IF NOT EXISTS todo(
-    id INTEGER PRIMARY kEY,
-    title TEXT,
-    completed TEXT
+DATABASE_URL="sqlite:///./test.db"
+engine= create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
 )
-""")
-conn.commit()
+Sessionlocal =sessionmaker(bind=engine)
 
-@app.get("/")
-def home():
-    return{
-        "message":"SQlite connected fine"
-    }
+Base = declarative_base()
+class Todo(Base):
+    __tablename__ ="todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(String)
+
+    Base.metadata.create_all(bind=engine)
+
+    def get_db():
+        db=Sessionlocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    @app.get("/")
+    def home(db:Session=Depends(get_db)):
+        return{
+            "message":"DB Connected Fine"
+        }
+
+
+
