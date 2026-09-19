ecd98462e892653a4a4c53b4fc0877ed4c650734
RoumesaShabbir45 <roumesa45@gmail.com>
2026-09-19 08:09:55 -0700

    authentication basic

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-from fastapi import FastAPI
-import time
-import asyncio
-
+from fastapi import FastAPI,HTTPException,Header,Depends
+from jose import jwt
+from datetime import datetime,timezone,timedelta
 
 app=FastAPI()
+#making secure key
+SECREAT_KEY= "mysecreat"
+ALGORITHM="HS256"
 
-@app.get("/")
-async def home():
-    await asyncio.sleep(3)
-    return {
-        "message":"Async Api"
+#create token function
+def create_token(data:dict):
+    to_encode=data.copy()
+    expire= datetime.now(timezone.utc)+timedelta(minutes=30)
+    to_encode.update({
+      "exp":expire
+})
+
+    token=jwt.encode(to_encode,SECREAT_KEY,algorithm=ALGORITHM)
+    return token
+
+#login api(token generate)
+@app.post("/login")
+def login(username:str ,password:str):
+    if username!="admin" or password!="123456":
+      raise HTTPException(
+        status_code=401,
+        detail="invalid usernmae and password"
+    )
+    token=create_token({
+       "sub":username
+    })
+    return{
+       "access_token":token
     }
+#verify token
+def varify_token(token: str = Header(None)):
+
+    try:
+        payload=jwt.decode(token,SECREAT_KEY,algorithms=[ALGORITHM])
+        return payload
+    except:
+        raise HTTPException(
+        status_code=401,
+        detail="invalid or expire token"
+        )
+#protected routes
+@app.get("/secure")
+def secure_data(user=Depends(varify_token)):
+    return{
+        "message":"secure data accessed",
+        "user":user
+    }
+
+        
